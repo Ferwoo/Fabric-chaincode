c2661ef1a7ce7968bea3e1b50e83db9d7b610619
Ferwoo <38686663+Ferwoo@users.noreply.github.com>
2019-04-28 11:26:09 +0800

    Update utils.go

--- utils/utils.go
+++ utils/utils.go
@@ -10,19 +10,20 @@ import (
 	"crypto/x509"
 )
 
-//获取当前操作智能合约成员的具体名称
+//获取当前操作智能合约的成员的具体名称
 func GetCreatorName(stub shim.ChaincodeStubInterface)(string,error)  {
 	name,err :=GetCreatorName(stub)//获取当前智能合约操作成员名称
 	if err !=nil{
 		return "",err
 	}
-
-	//格式化当前智能合约操作成员名称
+	
+//格式化当前智能合约的操作成员名称
 	memberName :=name[(strings.Index(name,"@")+1):strings.LastIndex(name,".example.com")]
 	return memberName,nil
 
 }
-//获取操作人员以及解析证书
+
+//获取操作人员以及解析相关的证书
 func GetCreator(stub shim.ChaincodeStubInterface)(string,error)  {
 	creatorByte,_:=stub.GetCreator()
 	certStart:=bytes.IndexAny(creatorByte,"------开始")
